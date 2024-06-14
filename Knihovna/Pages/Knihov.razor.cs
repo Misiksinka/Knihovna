51627e1d0a81856c4ff5670c593a0945ad914704
misach <misach@email.cz>
2024-06-14 15:27:20 +0200

    Přidejte soubory projektu.

---
+++ Knihovna/Pages/Knihov.razor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+namespace Knihovna.Pages
+{
+    public partial class Knihov
+    {
+        public Polozka Polozka { get; set; } = new Polozka();
+        public List <Polozka> PolozkaList { get; set; }= new List<Polozka>();
+        public List <Polozka> PolozkaFiltrList { get; set; }= new List<Polozka> ();
+
+        public bool JeEditace { get; set; } = false;
+        public bool JeFiltrace { get; set;} = false;
+
+        public double FiltrHodnota { get; set; }
+
+        private void Pridat (Microsoft.AspNetCore.Components.Web.MouseEventArgs e)
+        {
+            PolozkaList.Add(new Polozka(Polozka.Nazev, Polozka.Autor, Polozka.Rok, Polozka.Zanr, Polozka.ISBN, Polozka.Kusy));
+        }
+
+        private async Task SmazatZaznam(Polozka polozka)
+        {
+            string zprava = $"Chcete odebrat {polozka.Autor}{polozka.Nazev}{polozka.Rok}{polozka.Zanr}{polozka.ISBN}";
+            if (!await JS.InvokeAsync<bool>("confirm", zprava))
+            {
+
+            }
+            PolozkaList.Remove (polozka);
+        }
+        private Task <T> InvokeAsync<T>(string v, string zprava)
+        {
+            throw new NotImplementedException();
+        }
+        private void EditujZaznam(Polozka pol)
+        {
+            Polozka = pol;
+            JeEditace = true;
+        }
+
+        private void UkonciEditaci()
+        { 
+            JeEditace = false;
+            Polozka = new Polozka();
+        }
+        public void FiltrujPolozky()
+        {
+            PolozkaFiltrList = PolozkaList.Where(x=> x.ISBN==FiltrHodnota).ToList();
+        }
+
+    }
+}
---
+++ Knihovna/Polozka.cs
@@ -0,0 +1,33 @@
+﻿namespace Knihovna
+{
+    public class Polozka
+    {
+        private string nazev;
+        private string autor;
+        private int rok;
+        private string zanr;
+        private int isbn;
+        private int kusy;
+   
+
+        public Polozka()
+        {
+
+        }
+        public Polozka (string nazev, string autor, int rok, string zanr, int isbn, int kusy)
+        {
+            Nazev = nazev;
+            Autor = autor;
+            Rok = rok;
+            Zanr = zanr;
+            ISBN = isbn;
+            Kusy = kusy;
+        }
+        public string Nazev { get => nazev; set => nazev = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public int Rok { get => rok; set => rok = value; }
+        public string Zanr { get => zanr; set => zanr = value; }
+        public int ISBN { get => isbn; set => isbn = value; }
+        public int Kusy { get => kusy; set => kusy = value; }
+    }
+}
